b6a3e5159ade013345e6f5ae8168127f1c34f3ca
joyshilnil <charticchandra@gmail.com>
2022-10-25 23:49:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
+
+
+app.get('/', (req, res) => {
+    res.send('Course api running')
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+
+
+app.listen(port, () => {
+    console.log('Online Courses Server Running on port', port);
+
+})
